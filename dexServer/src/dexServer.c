63b06a1ddabbbe4a486ea0ab0648f6eab75aa915
Franco Consoni <franco.consoni@gmail.com>
2016-08-30 18:41:08 -0300

    Hecho el struct mayor que reconoce la estructura administrativa. Contiene las structs de los campos que la componen

--- dexServer/src/dexServer.c
+++ dexServer/src/dexServer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <commons/string.h>
 #include <commons/bitarray.h>
 
@@ -15,11 +16,38 @@ typedef struct{
 	char relleno[40];
 }Header;
 
+typedef struct{
+	char estado;
+	char nombre[17];
+	short int bloquePadre;
+	int tamanioArchivo;
+	char fecha[4];
+	int bloqueInicial;
+}TablaArchivos;
+
+typedef struct{
+	Header encabezado;
+	//bitarray
+	TablaArchivos TA[2048];
+	//tabla asignaciones
+}estructuraAdministrativa;
+
 int main(void) {
 
 	Header encab;
 
+	/*time_t tiempo = time(0);
+	struct tm *tlocal = localtime(&tiempo);
+	char fecha[5];
+	strftime(fecha,5,"%d%m",tlocal);*/
+
+	//con todo esto puedo sacar el dia y mes, necesario para el campo fecha de la tabla de archivos. Se va a usar mas tarde
+
+
 	printf("%d\n",sizeof(encab));
+	printf("%d\n",sizeof(TablaArchivos));
 	return 0;
 
 }
+
+
