19f3badf33c0759351a47b54e0c98f84dbe953d7
Franco Consoni <franco.conssoni@gmail.com>
2016-08-30 15:31:45 -0300

    Aca ya se reconoce la estructura del HEADER del FS

--- dexServer/src/dexServer.c
+++ dexServer/src/dexServer.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/string.h>
+#include <commons/bitarray.h>
+
+#define BLOCK_SIZE 64
+
+typedef struct{
+	char identificador[7];
+	char version;
+	int tamanioFS;
+	int tamanioBitmap;
+	int inicioTablaAsignaciones;
+	int tamanioDatos;
+	char relleno[40];
+}Header;
 
 int main(void) {
-	puts("Ejemplo commit");
-	return EXIT_SUCCESS;
+
+	Header encab;
+
+	printf("%d\n",sizeof(encab));
+	return 0;
+
 }
