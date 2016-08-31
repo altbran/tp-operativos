303961b6ce773731d0988461326b0ada24664cdd
ericstoppel <ericstoppel@hotmail.com>
2016-08-30 22:39:14 -0300

    Agrego IDDIBUJADORMAPA y cambios en dibujadorMapa.c

--- dibujadorMapa/src/dibujadorMapa.c
+++ dibujadorMapa/src/dibujadorMapa.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curses.h>
+#include <tad_items.h>
 #include <commons/log.h>
 #include <commons/collections/list.h>
 #include <nivel.h>
@@ -9,6 +10,19 @@
 #include <src/structs.h>
 
 int main(int argc, char **argv) {
+	int socket_mapa;
+	//me conecto al mapa
+	if (crearSocket(&socket_mapa)) {
+		printf("Error creando socket\n");
+		return 1;
+	}
+	/*if (conectarA(socket_mapa, IP_MAPA, PUERTO_MAPA)) {
+		printf("Error al conectar\n");
+		return 1;
+	}*/
+	if (responderHandshake(socket_mapa, IDDIBUJADORMAPA, IDMAPA)) {
+		return 1;
+	}
 
 	t_metadataPokenest pokenest1;
 	t_metadataPokenest pokenest2;
@@ -29,37 +43,37 @@ int main(int argc, char **argv) {
 	int rows, cols;
 	int q, p;
 
-	int x = 1;
-	int y = 1;
-
 	nivel_gui_inicializar();
-//	printf("ghola");
+
 	nivel_gui_get_area_nivel(&rows, &cols);
 
 	p = cols;
 	q = rows;
 
 	CrearPersonaje(items, '@', p, q);
-	CrearPersonaje(items, '#', 20, 20);
+	CrearPersonaje(items, '#', 10, 10);
 
-	CrearCaja(items, pokenest1.identificador, pokenest1.posicionX, pokenest1.posicionY, 5);
-	CrearCaja(items, pokenest2.identificador, pokenest2.posicionX, pokenest2.posicionY, 3);
-	CrearCaja(items, pokenest3.identificador, pokenest3.posicionX, pokenest3.posicionY, 2);
+	CrearCaja(items, pokenest1.identificador, pokenest1.posicionX,
+			pokenest1.posicionY, 5);
+	CrearCaja(items, pokenest2.identificador, pokenest2.posicionX,
+			pokenest2.posicionY, 3);
+	CrearCaja(items, pokenest3.identificador, pokenest3.posicionX,
+			pokenest3.posicionY, 2);
 
 	nivel_gui_dibujar(items, "Test Chamber 04");
 
-/*	BorrarItem(items, '#');
-	BorrarItem(items, '@');
+	/*	BorrarItem(items, '#');
+	 BorrarItem(items, '@');
 
-	BorrarItem(items, '1');
-	BorrarItem(items, '2');
+	 BorrarItem(items, '1');
+	 BorrarItem(items, '2');
 
-	BorrarItem(items, 'H');
-	BorrarItem(items, 'M');
-	BorrarItem(items, 'F');
-*/
-	//nivel_gui_terminar();
+	 BorrarItem(items, 'H');
+	 BorrarItem(items, 'M');
+	 BorrarItem(items, 'F');
 
+	 //nivel_gui_terminar();
 
+	 */
 	return EXIT_SUCCESS;
 }
