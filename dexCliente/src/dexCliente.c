396612e3ea2f5a9c3cefec5278c9e362acae599d
ericstoppel <ericstoppel@hotmail.com>
2016-09-08 11:32:34 -0300

    rename

--- dexCliente/src/dexCliente.c
+++ dexCliente/src/dexCliente.c
@@ -30,7 +30,8 @@ static int f_getattr(
 	t_privilegiosArchivo privilegios;
 	enviarHeader(S_POKEDEX_CLIENTE, privilegiosArchivo);
 	enviarPath(path, S_POKEDEX_CLIENTE);
-	recibirTodo(S_POKEDEX_CLIENTE, &privilegios, sizeof(privilegios));
+	recibirTodo(S_POKEDEX_CLIENTE,&privilegios.esDir,sizeof(int));
+	recibirTodo(S_POKEDEX_CLIENTE,&privilegios.tamanio,sizeof(uint32_t));
 	if (privilegios.esDir) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
@@ -51,7 +52,6 @@ static int f_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	int cantidadBytesARecibir;
 	char* cadenaARecibir;
 
-
 	enviarHeader(S_POKEDEX_CLIENTE, contenidoDirectorio);
 	enviarPath(path, S_POKEDEX_CLIENTE);
 
@@ -63,8 +63,9 @@ static int f_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		for (i = 0; i < cantidadArchivos; i++) {
 			cantidadBytesARecibir = recibirHeader(S_POKEDEX_CLIENTE);
 			cadenaARecibir = malloc(18);
-			recibirTodo(S_POKEDEX_CLIENTE, cadenaARecibir, cantidadBytesARecibir);/*Me lo manda X veces?*/
-			filler(buf,cadenaARecibir,NULL,0);
+			recibirTodo(S_POKEDEX_CLIENTE, cadenaARecibir,
+					cantidadBytesARecibir);/*Me lo manda X veces?*/
+			filler(buf, cadenaARecibir, NULL, 0);
 			free(cadenaARecibir);
 		}
 	}
@@ -93,8 +94,23 @@ static int f_write(const char *path, const char *path2, size_t size,
 	return 0;
 }
 
+static int f_rename(const char *pathAntiguo, const char *pathNuevo) {
+	t_cambioDeDirectorios estructura;
+	estructura.pathAntiguo = pathAntiguo;
+	estructura.pathNuevo = pathNuevo;
+	int cursorMemoria = 0;
+
+	void *buffer = malloc(sizeof(const char*) + sizeof(const char*)); //posx + posy
+	memcpy(buffer, &estructura.pathAntiguo, sizeof(const char*));
+	cursorMemoria += sizeof(const char*);
+	memcpy(buffer + cursorMemoria, &estructura.pathNuevo, sizeof(const char*));
+	send(S_POKEDEX_CLIENTE, buffer, sizeof(buffer), 0);
+	free(buffer);
+
+	return 0;
+}
 static struct fuse_operations ejemplo_oper = { .readdir = f_readdir, .getattr =
-		f_getattr, .read = f_read, .write = f_write, };
+		f_getattr, .read = f_read, .write = f_write, .rename = f_rename, };
 
 int main(int argc, char *argv[]) {
 	char *PUERTOSTR = getenv("PUERTO_POKEDEX_SERVIDOR");
