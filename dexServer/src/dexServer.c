0e9e972e559861ae4ec2edafc25c13bbdf0974b0
Franco Consoni <franco.consoni@gmail.com>
2016-09-13 23:01:47 -0300

    Agregada la estructura del select

--- dexServer/src/dexServer.c
+++ dexServer/src/dexServer.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>			 //armar el bitmap
+#include <stdlib.h>			 //
 #include <stdbool.h>         //
 #include <time.h>
 #include <commons/string.h>
 #include <commons/bitarray.h>
 #include <src/sockets.h>
+#include <pthread.h>
+#include <commons/log.h>
 
 
 #define BLOCK_SIZE 64
 #define FILENAME_LENGTH 17
+
 
 //Estructuras
 
@@ -51,12 +54,21 @@ void leerEstructurasAdministrativas(FILE* archivo,t_estructuraAdministrativa* es
 void actualizarBitmap(FILE* archivo,t_bitarray* pBitMap);
 
 
-
 int main(void) {
 
-	t_estructuraAdministrativa estructuraAdministrativa;
+	int PUERTO_DEX_SERVIDOR = 5000; //getenv("PUERTO_DEX_SERVIDOR");
+	//int IP_DEX_SERVIDOR = getenv("IP_DEX_SERVIDOR");
 	FILE* archivo;
+	t_log* logger = log_create("dexServer.log","DEXSERVER",0,log_level_from_string("INFO"));
+
+	t_estructuraAdministrativa estructuraAdministrativa;
 	int i;
+	int socketNuevo;
+	int paquete;
+	int listener;   //socket listener
+	int fdmax;     //file descriptor maximo
+	fd_set bolsaDeSockets;
+	fd_set bolsaAuxiliar;
 
 	/*time_t tiempo = time(0);
 	struct tm *tlocal = localtime(&tiempo);
@@ -65,6 +77,114 @@ int main(void) {
 
 	//con esto puedo sacar el dia y mes, necesario para el campo fecha de la tabla de archivos. Se va a usar mas tarde
 
+	archivo = fopen("fileSystem.dat","rb");
+
+	leerEstructurasAdministrativas(archivo,&estructuraAdministrativa);
+
+	fclose(archivo);
+
+
+	if (crearSocket(&listener)) {
+		log_error(logger, "Se produjo un error creando el socket servidor");
+		return 1;
+	}
+	if (escucharEn(listener, PUERTO_DEX_SERVIDOR)) {
+		printf("Error al conectar");
+		log_error(logger, "Se produjo un error escuchando en el socket listener");
+		return 1;
+	}
+
+	log_info(logger, "Se creó correctamente el socket servidor");
+	log_info(logger, "Escuchando nuevas conexiones");
+
+	struct sockaddr_in direccionCliente;
+
+	socketNuevo = listener;     //listening socket descriptor
+
+	FD_ZERO(&bolsaDeSockets);    //limpia los sets
+	FD_ZERO(&bolsaAuxiliar);
+
+	FD_SET(socketNuevo, &bolsaDeSockets);       //mete el socket en la bolsa
+
+	fdmax = listener;
+
+	while (1)
+	{
+		bolsaAuxiliar = bolsaDeSockets;
+		if (select(fdmax + 1, &bolsaAuxiliar, NULL, NULL, NULL) == -1)
+		{
+			perror("select");
+			return 1;
+		}
+
+		for (i = 0; i <= fdmax; i++)
+		{
+			if (FD_ISSET(i, &bolsaAuxiliar)) // we got one!!
+			{
+				if (i == listener)
+				{
+					// handle new connections
+					//Espera hasta que el hilo haya guardado el valor que se le paso como parametro
+					//antes de sobreEscribir la variable nuevaConexion
+					socketNuevo = aceptarConexion(listener, &direccionCliente);
+
+					if (socketNuevo > fdmax)
+						fdmax = socketNuevo;
+
+					int idRecibido = iniciarHandshake(socketNuevo, IPOKEDEXSERVER); //todo cambiar
+
+					switch (idRecibido)
+					{
+						case IDERROR:
+							log_info(logger, "Se desconecto el socket");
+							close(socketNuevo);
+							break;
+
+						case IDPOKEDEXCLIENTE:  //todo cambiar esto
+							FD_SET(socketNuevo, &bolsaDeSockets);
+
+							log_info(logger, "Nuevo dexCliente conectado, socket %d", socketNuevo);
+							//Maneja consola
+							break;
+
+						default:
+							close(socketNuevo);
+							log_error(logger, "Error en el handshake. Conexion inesperada");
+							break;
+					}
+				}
+				else
+				{  //aca mas adelante, voy a tener que crear hilos
+
+					recibirTodo(i,&paquete,4);
+					printf("%d\n",paquete);
+
+					//int header = recibirHeader(i);
+
+					/*switch(header)
+					{
+						case IDENTRENADOR:
+							recibirTodo(i,&paquete,4);
+							printf("%d\n",paquete);
+					}*/
+				}
+			}
+		} //for del select
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 	archivo = fopen("fileSystem.dat","rb+");
 
@@ -89,7 +209,7 @@ int main(void) {
 	for(i = 0;i<60;i++)
 		printf("Estado osadaFile: %d\n",estructuraAdministrativa.tablaArchivos[i].estado);
 	for(i = 0;i<60;i++)
-		printf("Valor tablaAsignaciones: %d\n",estructuraAdministrativa.tablaAsignaciones[i]);
+		printf("Valor tablaAsignaciones: %d\n",estructuraAdministrativa.tablaAsignaciones[i]);*/
 
 
 
@@ -98,6 +218,7 @@ int main(void) {
 	free(estructuraAdministrativa.tablaAsignaciones);
 	free(estructuraAdministrativa.tablaArchivos);
 
+	log_destroy(logger);
 	return 0;
 }
 
