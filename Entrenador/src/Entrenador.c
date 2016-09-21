b77d461a7814916ae3c717d013605e6e96f94b40
Juan Cruz <juancruzvieiro@yahoo.com.ar>
2016-09-21 00:44:59 -0300

    entrenador v2

--- Entrenador/src/Entrenador.c
+++ Entrenador/src/Entrenador.c
@@ -7,27 +7,35 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-/*#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-#include <netdb.h>
-#include <unistd.h>
-#include "commons/log.h"
-#include <src/sockets.h>
-#include <src/structs.h>*/
 #include "funcionesE.h"
 
-int main(int argc, char** argv){
 
-	nombre = argv[1];
-	ruta = argv[2];
+int main(int argc, char** argv){
 
 	if(argc != 3){
 		printf("Numero de parametros incorrectos");
 		log_error(logger,"Numero de parametros incorrectos",mensaje);
 		return 1;
 	}
+	char rutaMetadata[30];
+	char nombreRuta[15];
+	//FILE* fp;
+	strcpy(nombreRuta,argv[1]);
+	strcpy(rutaMetadata,argv[2]);
+
+	strcat(strcat(ruta,"/Entrenadores/"), nombre);
+
+	//fp = fopen(rutaMetadata,"r");
+	t_config* metaDataEntrenador = config_create(rutaMetadata);
+	entrenador.nombre  = config_get_string_value(metaDataEntrenador, "nombre");
+
+	char* simbolo = config_get_string_value(metaDataEntrenador, "simbolo");
+	entrenador.simbolo = simbolo[0];
+	entrenador.vidas = config_get_int_value(metaDataEntrenador, "vidas");
+
+	entrenador.reintentos = config_get_int_value(metaDataEntrenador, "reintentos");
+
 
 	char* IP_MAPA_SERVIDOR = getenv("IP_MAPA_SERVIDOR");
 	logger = log_create("Entrenador.log", "ENTRENADOR", 0, log_level_from_string("INFO"));
