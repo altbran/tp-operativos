cb4c90f6577eac66310cf3a656c949efa3026196
lucascharlab <lucascharlab@gmail.com>
2016-10-29 16:20:47 -0300

    arreglo ultimo header

--- LibreriasSO/src/protocolo.h
+++ LibreriasSO/src/protocolo.h
@@ -15,7 +15,7 @@ enum headers { //Constantes que identifican los headers de los mensajes
 	movimientoAceptado= 15,
 	mejorPokemon = 19,
 	notificarDeadlock= 20,
-	notificarResultadoBatalla,
+	notificarResultadoBatalla = 21,
 	//Headers para comunicacion entre cliente y servidor pokedex
 	privilegiosArchivo = 4,
 	contenidoDirectorio = 5,
